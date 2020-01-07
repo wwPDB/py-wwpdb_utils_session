494d575b33f5d08b841754eb3f7f14a8dd436be4
Ezra Peisach <ezra.peisach@rcsb.org>
2020-01-07 01:40:00 -0500

    Full test coverage of SessionManager

--- wwpdb/utils/tests-session/SessionManagerTests.py
+++ wwpdb/utils/tests-session/SessionManagerTests.py
@@ -12,14 +12,22 @@ __author__ = "Ezra Peisach"
 __email__ = "peisach@rcsb.rutgers.edu"
 __license__ = "Creative Commons Attribution 3.0 Unported"
 __version__ = "V0.01"
+
+import os
 import unittest
+import platform
+import shutil
 
 from wwpdb.utils.session.SessionManager import SessionManager
 
 
 class SessionTests(unittest.TestCase):
     def setUp(self):
-        pass
+        HERE = os.path.abspath(os.path.dirname(__file__))
+        TESTOUTPUT = os.path.join(HERE, "test-output", platform.python_version())
+        if not os.path.exists(TESTOUTPUT):  # pragma: no cover
+            os.makedirs(TESTOUTPUT)
+        self.__sessiontop = TESTOUTPUT
 
     def testId(self):
         """Tests generation of session id for python"""
@@ -32,7 +40,39 @@ class SessionTests(unittest.TestCase):
 
         self.assertNotEqual(sm.assignId(), None, "Failed to return id")
         self.assertNotEqual(sm.getId(), "12345", "Failed to generate new id")
+        self.assertIn("SessionManager()", str(sm), "Failed to get string representation")
+        self.assertIn("SessionManager()", repr(sm), "Failed to get repr representation")
+        self.assertIsNotNone(sm.getTopPath(), "Top path should not be none")
+        self.assertIsNotNone(sm.getRelativePath(), "Relative path should not be none")
+
+    def testBadPath(self):
+        """Tests bad path access"""
+        here = os.path.abspath(os.path.dirname(__file__))
+        badpath = os.path.join(here, "bad_dir_for_sessions")
+        sm = SessionManager(badpath)
+        self.assertIsNone(sm.getPath(), "Expected path should be None")
+
+        # Test with path of None - should error out
+        sm = SessionManager(None)
+        self.assertIsNone(sm.getPath(), "Expected path should be None")
+        self.assertIsNone(sm.getTopPath(), "Top path expected to be none")
+
+    def testSessionPathCreation(self):
+        """Tests creation of session dir"""
+        sessdir = os.path.join(self.__sessiontop, "sesscreate")
+        if os.path.exists(sessdir):  # pragma: no cover
+            shutil.rmtree(sessdir)
+        sm = SessionManager(sessdir, verbose=True)
+        # Fail without uid set
+        self.assertIsNone(sm.makeSessionPath(), "Creating session path uid not set")
+        self.assertIsNone(sm.remakeSessionPath(), "Creating session path uid not set")
+        self.assertIsNone(sm.getRelativePath(), "Relative path should be none with uid not set")
+
+        self.assertIsNotNone(sm.assignId())
+        self.assertIsNotNone(sm.makeSessionPath(), "Creating session path uid set")
+        self.assertIsNotNone(sm.remakeSessionPath(), "Creating session path uid set")
+        self.assertIsNotNone(sm.getPath(), "Expected path should not be None")
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: no cover
     unittest.main()
