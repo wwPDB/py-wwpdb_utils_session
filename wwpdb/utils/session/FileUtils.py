21293abb9b5283055e5ab775b850adfeef6c109c
jmb <jmb@ebi.ac.uk>
2021-07-16 08:36:00 +0100

    auto pycharm clean up

--- wwpdb/utils/session/FileUtils.py
+++ wwpdb/utils/session/FileUtils.py
@@ -21,10 +21,10 @@ __email__ = "jwest@rcsb.rutgers.edu"
 __license__ = "Creative Commons Attribution 3.0 Unported"
 __version__ = "V0.07"
 
-import sys
-import os.path
-import os
 import glob
+import os
+import os.path
+import sys
 
 from wwpdb.io.file.DataExchange import DataExchange
 from wwpdb.io.locator.PathInfo import PathInfo
@@ -35,7 +35,8 @@ class FileUtilsBase(object):
     """Base class that defines the content types to download"""
 
     def __init__(self):
-        self._rDList = ["Primary Data Files", "Chemical Assignment Files", "Sequence Assignment Files", "Annotation Task Files", "Check reports", "Message Files", "3DEM Files"]
+        self._rDList = ["Primary Data Files", "Chemical Assignment Files", "Sequence Assignment Files",
+                        "Annotation Task Files", "Check reports", "Message Files", "3DEM Files"]
         self._rD = {
             "Primary Data Files": [
                 "model",
@@ -109,7 +110,8 @@ class FileUtilsBase(object):
                 "fsc-report",
                 "em2em-report",
             ],
-            "Message Files": ["messages-from-depositor", "messages-to-depositor", "notes-from-annotator", "correspondence-to-depositor"],
+            "Message Files": ["messages-from-depositor", "messages-to-depositor", "notes-from-annotator",
+                              "correspondence-to-depositor"],
             "3DEM Files": [
                 "em-volume",
                 "em-mask-volume",
@@ -140,7 +142,8 @@ class FileUtils(FileUtilsBase):
         self.__entryId = entryId
         siteId = self.__reqObj.getValue("WWPDB_SITE_ID")
         # This is for viewing the entries from the standalone validation server from annotation --
-        if siteId in ["WWPDB_DEPLOY_PRODUCTION_RU", "WWPDB_DEPLOY_VALSRV_RU", "WWPDB_DEPLOY_TEST", "WWPDB_DEPLOY_INTERNAL_RU"] and entryId.startswith("D_90"):
+        if siteId in ["WWPDB_DEPLOY_PRODUCTION_RU", "WWPDB_DEPLOY_VALSRV_RU", "WWPDB_DEPLOY_TEST",
+                      "WWPDB_DEPLOY_INTERNAL_RU"] and entryId.startswith("D_90"):
             siteId = "WWPDB_DEPLOY_VALSRV_RU"
         #
         # Get inventory of file types
@@ -154,12 +157,14 @@ class FileUtils(FileUtilsBase):
         else:
             self.__siteId = self.__reqObj.getValue("WWPDB_SITE_ID")
         #
-        self.__lfh.write("+FileUtils.__setup() starting with entryId %r adjusted WWPDB_SITE_ID %r\n" % (self.__entryId, self.__siteId))
+        self.__lfh.write("+FileUtils.__setup() starting with entryId %r adjusted WWPDB_SITE_ID %r\n" % (
+        self.__entryId, self.__siteId))
         #
         self.__sObj = self.__reqObj.getSessionObj()
         self.__sessionId = self.__sObj.getId()
         self.__sessionPath = self.__sObj.getPath()
-        self.__pI = PathInfo(siteId=self.__siteId, sessionPath=self.__sessionPath, verbose=self.__verbose, log=self.__lfh)
+        self.__pI = PathInfo(siteId=self.__siteId, sessionPath=self.__sessionPath, verbose=self.__verbose,
+                             log=self.__lfh)
         self.__cI = ConfigInfo(self.__siteId)
         self.__msL = self.__cI.get("CONTENT_MILESTONE_LIST")
         #
@@ -184,7 +189,8 @@ class FileUtils(FileUtilsBase):
                         mt = ct + "-" + ms
                         fList.append(mt)
                 nF, oL = self.__renderContentTypeFileList(
-                    self.__entryId, fileSource=fileSource, wfInstanceId=None, contentTypeList=fList, title=title, displayImageFlag=displayImageFlag
+                    self.__entryId, fileSource=fileSource, wfInstanceId=None, contentTypeList=fList, title=title,
+                    displayImageFlag=displayImageFlag
                 )
                 if nF > 0:
                     htmlList.extend(oL)
@@ -217,15 +223,18 @@ class FileUtils(FileUtilsBase):
         #
         return nTot, htmlList
 
-    def __renderContentTypeFileList(self, entryId, fileSource="archive", wfInstanceId=None, contentTypeList=None, title=None, displayImageFlag=False):
+    def __renderContentTypeFileList(self, entryId, fileSource="archive", wfInstanceId=None, contentTypeList=None,
+                                    title=None, displayImageFlag=False):
         if contentTypeList is None:
             contentTypeList = ["model"]
         if self.__verbose:
             self.__lfh.write(
-                "+FileUtils.renderContentTypeFileList() entryId %r fileSource %r wfInstanceId %r contentTypeList %r \n" % (entryId, fileSource, wfInstanceId, contentTypeList)
+                "+FileUtils.renderContentTypeFileList() entryId %r fileSource %r wfInstanceId %r contentTypeList %r \n" % (
+                entryId, fileSource, wfInstanceId, contentTypeList)
             )
         de = DataExchange(
-            reqObj=self.__reqObj, depDataSetId=entryId, wfInstanceId=wfInstanceId, fileSource=fileSource, siteId=self.__siteId, verbose=self.__verbose, log=self.__lfh
+            reqObj=self.__reqObj, depDataSetId=entryId, wfInstanceId=wfInstanceId, fileSource=fileSource,
+            siteId=self.__siteId, verbose=self.__verbose, log=self.__lfh
         )
         tupL = de.getContentTypeFileList(fileSource=fileSource, contentTypeList=contentTypeList)
         #
@@ -260,7 +269,8 @@ class FileUtils(FileUtilsBase):
             self.__lfh.write("+FileUtils.renderWfInstanceFileList() wfPath %s\n" % wfPath)
 
         wfPattern = os.path.join(wfPath, "*")
-        de = DataExchange(reqObj=self.__reqObj, depDataSetId=entryId, wfInstanceId=None, fileSource=None, siteId=self.__siteId, verbose=self.__verbose, log=self.__lfh)
+        de = DataExchange(reqObj=self.__reqObj, depDataSetId=entryId, wfInstanceId=None, fileSource=None,
+                          siteId=self.__siteId, verbose=self.__verbose, log=self.__lfh)
         tupL = de.getMiscFileList(fPatternList=[wfPattern], sortFlag=True)
         #
         rTupL = []
@@ -282,7 +292,8 @@ class FileUtils(FileUtilsBase):
     def __renderLogFileList(self, entryId, fileSource="archive", title=None):
         if self.__verbose:
             self.__lfh.write("+FileUtils.renderLogFileList() entryId %r fileSource %r\n" % (entryId, fileSource))
-        de = DataExchange(reqObj=self.__reqObj, depDataSetId=entryId, wfInstanceId=None, fileSource=fileSource, siteId=self.__siteId, verbose=self.__verbose, log=self.__lfh)
+        de = DataExchange(reqObj=self.__reqObj, depDataSetId=entryId, wfInstanceId=None, fileSource=fileSource,
+                          siteId=self.__siteId, verbose=self.__verbose, log=self.__lfh)
         tupL = de.getLogFileList(entryId, fileSource=fileSource)
         #
         rTupL = []
@@ -307,11 +318,13 @@ class FileUtils(FileUtilsBase):
         if len(fileTupleList) > 0:
             if embeddedTitle:
                 oL.append('<table class="table table-bordered table-striped table-condensed">')
-                oL.append('<tr><th class="width50">%s</th><th class="width30">Modification Time</th><th class="width20">Size (KBytes)</th></tr>' % title)
+                oL.append(
+                    '<tr><th class="width50">%s</th><th class="width30">Modification Time</th><th class="width20">Size (KBytes)</th></tr>' % title)
             else:
                 oL.append("<h4>%s</h4>" % title)
                 oL.append('<table class="table table-bordered table-striped table-condensed">')
-                oL.append('<tr><th class="width50">Files</th><th class="width30">Modification Time</th><th class="width20">Size (KBytes)</th></tr>')
+                oL.append(
+                    '<tr><th class="width50">Files</th><th class="width30">Modification Time</th><th class="width20">Size (KBytes)</th></tr>')
             for tup in fileTupleList:
                 oL.append("<tr>")
                 if tup[0] == "displayImage":
