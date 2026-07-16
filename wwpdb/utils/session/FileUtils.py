bd75a1cf3a184e270eda2c744a485ae0543e7e27
Deborah Harrus <dharrus@ebi.ac.uk>
2026-07-16 12:13:08 +0100

    DAOTHER-10891

--- wwpdb/utils/session/FileUtils.py
+++ wwpdb/utils/session/FileUtils.py
@@ -9,6 +9,7 @@
 # 29- Nov-2016  ep  Add dict-check-report-next to list
 # 13- Feb-2016  ep  Add '3DEM Files' to default list for FileUtils.
 # 28-Sept-2017  zf  Modified renderFileList() & __renderContentTypeFileList()
+# 16-Jul-2026       Add xml-check-report, emd-xml-header-report, em-map-check-report
 ##
 """
 Manage the presentation of project files for download.
@@ -119,6 +120,9 @@ class FileUtilsBase:
                 "mapfix-report",
                 "fsc-report",
                 "em2em-report",
+                "xml-check-report",
+                "emd-xml-header-report",
+                "em-map-check-report",
             ],
             "Message Files": [
                 "messages-from-depositor",
